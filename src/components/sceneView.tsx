04a48ee75f96e705346f30a9bb34be545b2cfd01
piechota <piechota33@gmail.com>
2023-12-20 22:22:08 +0100

    scene view to function

--- src/components/sceneView.tsx
+++ src/components/sceneView.tsx
@@ -1,27 +1,12 @@
-import React from 'react';
 import './css/sceneView.css';
 
-class SceneViewProps {
-}
-
-class SceneViewState {
-}
-
-export class SceneView  extends React.Component<SceneViewProps, SceneViewState> {
-    
-    constructor(props: SceneViewProps) {
-        super(props);
-        this.state = new SceneViewState();
-    }
-
-    render(): React.ReactNode {
-        return (
-            <div className="scene-view-container">
-                <input className="scene-view-title" type="text" id="title" name="title" placeholder='Title'/><br/>
-                <img className="scene-view-img" src="tmp_img/img0.jpg" alt="Girl in a jacket"></img>
-                <img className="scene-view-img" src="tmp_img/img1.jpg" alt="Girl in a jacket"></img>
-                <img className="scene-view-img" src="tmp_img/img2.JPG" alt="Girl in a jacket"></img>
-            </div>
-        )
-    }
+export function SceneView() {
+    return (
+        <div className="scene-view-container">
+            <input className="scene-view-title" type="text" id="title" name="title" placeholder='Title'/><br/>
+            <img className="scene-view-img" src="tmp_img/img0.jpg" alt="Girl in a jacket"></img>
+            <img className="scene-view-img" src="tmp_img/img1.jpg" alt="Girl in a jacket"></img>
+            <img className="scene-view-img" src="tmp_img/img2.JPG" alt="Girl in a jacket"></img>
+        </div>
+    )
 }
